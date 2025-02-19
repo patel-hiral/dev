97ae4d1677be649f9797fd80b95bab4522d99b2e
Hiral Patel <hiral.patel@differenzsystem.com>
2025-02-19 17:45:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-export default function App() {
-  return (
-    <div className="main">
-      <h1>Hello There, It Works Fine</h1>
-=======
 import './App.css'
 export default function App() {
   return (
     <div className="main">
       <h1>Hello There, It Works on Production</h1>
->>>>>>> prod_repo/main
     </div>
   )
 }
