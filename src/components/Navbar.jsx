526bb0f410269269bb3f9bd293c7b958b0c045ce
Hiral Patel <hiral.patel@differenzsystem.com>
2025-02-20 15:29:11 +0530

    updated navbar again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Added Navbar on Feat/Navbar Branch(Updated)</div>
+    <div>Added Navbar on Feat/Navbar Branch(Updated Again)</div>
   )
 }
