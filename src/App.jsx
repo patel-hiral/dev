1b9e2636b7c941eac4c0aa550dbc28e98883cb3b
Hiral Patel <hiral.patel@differenzsystem.com>
2025-02-20 15:24:28 +0530

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 export default function App() {
   return (
     <div className="main">
-      <Navbar/>
+      <Navbar />
       <h1>Hello There, It Works on DEV</h1>
+      <Footer />
     </div>
   )
 }
